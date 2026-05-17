1664beb505c913d4122290d32bdcdde260967d13
Lascelles <waunaneville@gmail.com>
2026-05-17 16:16:01 +0300

    Import ToyCard and render mapped toys in ToyContainer

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -5,15 +5,4 @@ function ToyContainer({ toys, onDeleteToy, onUpdateToy }) {
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
-        <ToyCard
-          key={toy.id}
-          toy={toy}
-          onDeleteToy={onDeleteToy}
-          onUpdateToy={onUpdateToy}
-        />
-      ))}
-    </div>
-  );
-}
-
-export default ToyContainer;
+       
