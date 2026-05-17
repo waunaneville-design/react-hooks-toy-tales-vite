6631b0224fdb6f587ffcecb0a45315478d010233
Lascelles <waunaneville@gmail.com>
2026-05-17 16:08:21 +0300

    Implement PATCH request to update toy likes on server

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -6,4 +6,11 @@ function ToyCard({ toy, onDeleteToy, onUpdateToy }) {
   function handleLike() {
     const updatedLikes = toy.likes + 1;
 
-   
+   fetch(`${TOYS_URL}/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: updatedLikes }),
+    })
+ 
