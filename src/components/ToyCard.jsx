e61509e6cbb7d21a7400a513a674ff6a24cd5a71
Lascelles <waunaneville@gmail.com>
2026-05-17 16:07:20 +0300

    Add handleLike in ToyCard to increment toy likes

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -6,45 +6,4 @@ function ToyCard({ toy, onDeleteToy, onUpdateToy }) {
   function handleLike() {
     const updatedLikes = toy.likes + 1;
 
-    fetch(`${TOYS_URL}/${toy.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ likes: updatedLikes }),
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error("Could not update toy likes");
-        return response.json();
-      })
-      .then(onUpdateToy)
-      .catch(console.error);
-  }
-
-  function handleDelete() {
-    fetch(`${TOYS_URL}/${toy.id}`, {
-      method: "DELETE",
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error("Could not delete toy");
-        onDeleteToy(toy.id);
-      })
-      .catch(console.error);
-  }
-
-  return (
-    <div className="card" data-testid="toy-card">
-      <h2>{toy.name}</h2>
-      <img src={toy.image} alt={toy.name} className="toy-avatar" />
-      <p>{toy.likes} Likes </p>
-      <button className="like-btn" onClick={handleLike}>
-        Like {"<3"}
-      </button>
-      <button className="del-btn" onClick={handleDelete}>
-        Donate to GoodWill
-      </button>
-    </div>
-  );
-}
-
-export default ToyCard;
+   
